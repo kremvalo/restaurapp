9fd709a7efcb33ca69875bee04f4ec45b65c3489
kremvalo <kren.thinker@gmail.com>
2023-09-27 12:08:20 -0500

    first charge

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './src/framework/presentation/components/home';
+
+const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default App;
